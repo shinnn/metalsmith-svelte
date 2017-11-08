a07cd09646a88c5c577d87253ead60f1f426870e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-08 09:25:25 +0900

    update dependencies and devDependencies
    Highlight: ESLint v3 -> v4
    
    https://eslint.org/blog/2017/06/eslint-v4.0.0-released
    https://eslint.org/docs/user-guide/migrating-to-4.0.0

--- test.js
+++ test.js
@@ -15,14 +15,14 @@ test('metalsmith-svelte', t => {
     'source.html': {contents: Buffer.from('<div />')},
     'non-html.txt': {contents: Buffer.from('Hi')}
   }, (err, files) => {
-    t.strictEqual(err, null, 'should be used as a metalsmith plugin.');
+    t.equal(err, null, 'should be used as a metalsmith plugin.');
 
-    t.strictEqual(
-      String(files['source.js'].contents).split('\n')[0],
-      'function create_main_fragment ( state, component ) {',
+    t.equal(
+      String(files['source.js'].contents).split('\n')[2],
+      'function create_main_fragment(state, component) {',
       'should compile a file with Svelte compiler.'
     );
-    t.strictEqual(
+    t.equal(
       String(files['non-html.txt'].contents),
       'Hi',
       'should not transform non-HTML files.'
@@ -36,18 +36,18 @@ test('metalsmith-svelte', t => {
     generate: 'ssr',
     sourceMap: true
   }))
-  .run({'source.html': {contents: new Buffer(0)}}, (err, files) => {
-    t.strictEqual(err, null, 'should accept an empty file.');
+  .run({'source.html': {contents: Buffer.alloc(0)}}, (err, files) => {
+    t.equal(err, null, 'should accept an empty file.');
 
     const contents = files['source.js'].contents.toString();
 
-    t.strictEqual(
+    t.equal(
       contents.split('\n').splice(-3)[0],
       'export default GulpSvelteTest;',
       'should support Svelte compiler options.'
     );
 
-    t.strictEqual(
+    t.equal(
       contents.split('\n').splice(-2)[0],
       '//# sourceMappingURL=source.js.map',
       'should append source map URL when `sourceMap` option is \'inline\'.'
@@ -61,7 +61,7 @@ test('metalsmith-svelte', t => {
         sources: [join(__dirname, 'src', 'source.html')],
         sourcesContent: [''],
         names: [],
-        mappings: ';'.repeat(36)
+        mappings: ';'.repeat(35)
       },
       'should generate valid source map.'
     );
@@ -70,10 +70,10 @@ test('metalsmith-svelte', t => {
   new Metalsmith('.')
   .use(svelte({sourceMap: 'inline'}))
   .run({'☺️.html': {contents: Buffer.from('<div />')}}, (err, files) => {
-    t.strictEqual(err, null, 'should support non-ASCII filename.');
-    t.strictEqual(
+    t.equal(err, null, 'should support non-ASCII filename.');
+    t.equal(
       String(files['☺️.js'].contents).split('\n').splice(-2)[0],
-      `//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjpudWxsLCJzb3VyY2VzIjpbIi9Vc2Vycy9zaGlubm4vZ2l0aHViL21ldGFsc21pdGgtc3ZlbHRlL3NyYy/imLrvuI8uaHRtbCJdLCJzb3VyY2VzQ29udGVudCI6WyI8ZGl2IC8+Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI${'7Ozs'.repeat(59)}7OyJ9`,
+      `//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjpudWxsLCJzb3VyY2VzIjpbIi9Vc2Vycy9zaGlubm4vZ2l0aHViL21ldGFsc21pdGgtc3ZlbHRlL3NyYy/imLrvuI8uaHRtbCJdLCJzb3VyY2VzQ29udGVudCI6WyI8ZGl2IC8+Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI${'7Ozs'.repeat(71)}7In0=`,
       'should append Base64-encoded source map when `sourceMap` option is \'inline\'.'
     );
   });
@@ -82,13 +82,13 @@ test('metalsmith-svelte', t => {
   .source('custom_source_directory')
   .use(svelte({sourceMap: false}))
   .run({'FOO.HTML': {contents: Buffer.from('</>')}}, ({name, filename}) => {
-    t.strictEqual(
+    t.equal(
       name,
       'ParseError',
       'should fail when svelte cannot transpile the code.'
     );
 
-    t.strictEqual(
+    t.equal(
       filename,
       join(__dirname, 'custom_source_directory', 'FOO.HTML'),
       'should include filename to the error object.'
