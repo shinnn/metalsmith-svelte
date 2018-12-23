9b3a705918b846f91f2243b5399e5725114aa1e2
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-12-24 00:52:18 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update Svelte for v1 to v2
    * update ESLint from v4.x to v5.x
    * update @shinnn/eslint-config and follow its rules, for example tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -5,72 +5,75 @@ const {dirname, extname, join, relative} = require('path');
 const {compile} = require('svelte');
 const inspectWithKind = require('inspect-with-kind');
 const isPlainObj = require('is-plain-obj');
-const sourceMapToComment = require('source-map-to-comment');
 const toFastProperties = require('to-fast-properties');
 
+const SOURCE_MAP_COMMENT_PREFIX = '//# sourceMappingURL=';
+
 module.exports = function metalsmithSvelte(...args) {
-  const argLen = args.length;
+	const argLen = args.length;
 
-  if (argLen > 1) {
-    const error = new RangeError(`Expected 0 or 1 argument (<Object>), but got ${argLen} arguments.`);
-    error.code = 'ERR_TOO_MANY_ARGS';
+	if (argLen > 1) {
+		const error = new RangeError(`Expected 0 or 1 argument (<Object>), but got ${argLen} arguments.`);
+		error.code = 'ERR_TOO_MANY_ARGS';
 
-    throw error;
-  }
+		throw error;
+	}
 
-  const [options = {}] = args;
+	const [options = {}] = args;
 
-  if (argLen === 1) {
-    if (!isPlainObj(options)) {
-      const error = new TypeError(`Expected an <Object> to set Svelte compiler options, but got ${
-        inspectWithKind(options)
-      }.`);
-      error.code = 'ERR_INVALID_ARG_TYPE';
+	if (argLen === 1) {
+		if (!isPlainObj(options)) {
+			const error = new TypeError(`Expected an <Object> to set Svelte compiler options, but got ${
+				inspectWithKind(options)
+			}.`);
+			error.code = 'ERR_INVALID_ARG_TYPE';
 
-      throw error;
-    }
+			throw error;
+		}
 
-    if (typeof options.sourceMap !== 'boolean' && options.sourceMap !== 'inline') {
-      const error = new TypeError(`Expected \`sourceMap\` option to be true, false or 'inline', but got ${
-        inspectWithKind(options.sourceMap)
-      }.`);
-      error.code = 'ERR_INVALID_OPT_VALUE';
+		if (typeof options.sourceMap !== 'boolean' && options.sourceMap !== 'inline') {
+			const error = new TypeError(`Expected \`sourceMap\` option to be true, false or 'inline', but got ${
+				inspectWithKind(options.sourceMap)
+			}.`);
+			error.code = 'ERR_INVALID_OPT_VALUE';
 
-      throw error;
-    }
-  }
+			throw error;
+		}
+	}
 
-  return function metalsmithBublePlugin(files, metalsmith) {
-    for (const originalFilename of Object.keys(files)) {
-      const ext = extname(originalFilename).toLowerCase();
-      if (ext !== '.htm' && ext !== '.html' && ext !== '.svelte') {
-        return;
-      }
+	return function metalsmithBublePlugin(files, metalsmith) {
+		for (const originalFilename of Object.keys(files)) {
+			const ext = extname(originalFilename).slice(1).toLowerCase();
+			if (ext !== 'htm' && ext !== 'html' && ext !== 'svelte') {
+				return;
+			}
 
-      const filename = originalFilename.replace(/\.(html?|svelte)$/i, '.js');
+			const filename = originalFilename.replace(/\.(html?|svelte)$/ui, '.js');
 
-      const result = compile(files[originalFilename].contents.toString(), Object.assign({}, options, {
-        filename: join(metalsmith.source(), originalFilename)
-      }));
+			const {js} = compile(files[originalFilename].contents.toString(), Object.assign({}, options, { // eslint-disable-line prefer-object-spread
+				filename: join(metalsmith.source(), originalFilename)
+			}));
 
-      if (options.sourceMap === true) {
-        const sourcemapPath = `${filename}.map`;
-        files[sourcemapPath] = {
-          contents: Buffer.from(JSON.stringify(result.map))
-        };
+			if (options.sourceMap === true) {
+				const sourcemapPath = `${filename}.map`;
+				files[sourcemapPath] = {
+					contents: Buffer.from(JSON.stringify(js.map))
+				};
 
-        result.code += `\n//# sourceMappingURL=${
-          relative(dirname(filename), sourcemapPath).replace(/\\/g, '/')
-        }\n`;
-      } else if (options.sourceMap === 'inline') {
-        result.code += `\n${sourceMapToComment(result.map)}\n`;
-      }
+				js.code += `
+${SOURCE_MAP_COMMENT_PREFIX}${relative(dirname(filename), sourcemapPath).replace(/\\/ug, '/')}
+`;
+			} else if (options.sourceMap === 'inline') {
+				js.code += `
+${SOURCE_MAP_COMMENT_PREFIX}data:application/json;base64,${Buffer.from(JSON.stringify(js.map))}
+`;
+			}
 
-      files[filename] = files[originalFilename];
-      delete files[originalFilename];
-      toFastProperties(files);
+			files[filename] = files[originalFilename];
+			delete files[originalFilename];
+			toFastProperties(files);
 
-      files[filename].contents = Buffer.from(result.code);
-    }
-  };
+			files[filename].contents = Buffer.from(js.code);
+		}
+	};
 };
--- test.js
+++ test.js
@@ -7,112 +7,112 @@ const metalsmithSvelte = require('.');
 const test = require('tape');
 
 test('metalsmith-svelte', t => {
-  t.plan(11);
+	t.plan(11);
 
-  new Metalsmith('.')
-  .use(metalsmithSvelte())
-  .run({
-    'source.html': {contents: Buffer.from('<div />')},
-    'non-html.txt': {contents: Buffer.from('Hi')}
-  }, (err, files) => {
-    t.equal(err, null, 'should be used as a metalsmith plugin.');
+	new Metalsmith('.')
+	.use(metalsmithSvelte())
+	.run({
+		'source.html': {contents: Buffer.from('<div />')},
+		'non-html.txt': {contents: Buffer.from('Hi')}
+	}, (err, files) => {
+		t.equal(err, null, 'should be used as a metalsmith plugin.');
 
-    t.equal(
-      String(files['source.js'].contents).split('\n')[2],
-      'function create_main_fragment(component, state) {',
-      'should compile a file with Svelte compiler.'
-    );
-    t.equal(
-      String(files['non-html.txt'].contents),
-      'Hi',
-      'should not transform non-HTML files.'
-    );
-  });
+		t.equal(
+			String(files['source.js'].contents).split('\n')[2],
+			'function create_main_fragment(component, ctx) {',
+			'should compile a file with Svelte compiler.'
+		);
+		t.equal(
+			String(files['non-html.txt'].contents),
+			'Hi',
+			'should not transform non-HTML files.'
+		);
+	});
 
-  new Metalsmith('.')
-  .use(metalsmithSvelte({
-    name: 'GulpSvelteTest',
-    format: 'es',
-    generate: 'ssr',
-    sourceMap: true
-  }))
-  .run({'source.htm': {contents: Buffer.alloc(0)}}, (err, files) => {
-    t.equal(err, null, 'should accept an empty file.');
+	new Metalsmith('.')
+	.use(metalsmithSvelte({
+		name: 'GulpSvelteTest',
+		format: 'es',
+		generate: 'ssr',
+		sourceMap: true
+	}))
+	.run({'source.htm': {contents: Buffer.alloc(0)}}, (err, files) => {
+		t.equal(err, null, 'should accept an empty file.');
 
-    const contents = files['source.js'].contents.toString();
+		const contents = files['source.js'].contents.toString();
 
-    t.equal(
-      contents.split('\n').splice(-3)[0],
-      'export default GulpSvelteTest;',
-      'should support Svelte compiler options.'
-    );
+		t.equal(
+			contents.split('\n').splice(-3)[0],
+			'export default GulpSvelteTest;',
+			'should support Svelte compiler options.'
+		);
 
-    t.equal(
-      contents.split('\n').splice(-2)[0],
-      '//# sourceMappingURL=source.js.map',
-      'should append source map URL when `sourceMap` option is \'inline\'.'
-    );
+		t.equal(
+			contents.split('\n').splice(-2)[0],
+			'//# sourceMappingURL=source.js.map',
+			'should append source map URL when `sourceMap` option is \'inline\'.'
+		);
 
-    t.deepEqual(
-      JSON.parse(files['source.js.map'].contents),
-      {
-        version: 3,
-        file: null,
-        sources: [join(__dirname, 'src', 'source.htm')],
-        sourcesContent: [''],
-        names: [],
-        mappings: ';'.repeat(58)
-      },
-      'should generate valid source map.'
-    );
-  });
+		t.deepEqual(
+			JSON.parse(files['source.js.map'].contents),
+			{
+				version: 3,
+				file: null,
+				sources: [join(__dirname, 'src', 'source.htm')],
+				sourcesContent: [''],
+				names: [],
+				mappings: ';'.repeat(44)
+			},
+			'should generate valid source map.'
+		);
+	});
 
-  new Metalsmith('.')
-  .use(metalsmithSvelte({sourceMap: 'inline'}))
-  .run({'☺️.svelte': {contents: Buffer.from('<div />')}}, (err, files) => {
-    t.equal(err, null, 'should support .svelte file extension.');
-    t.ok(
-      String(files['☺️.js'].contents).split('\n').splice(-2)[0].startsWith('//# sourceMappingURL=data:application/json;base64,'),
-      'should append Base64-encoded source map when `sourceMap` option is \'inline\'.'
-    );
-  });
+	new Metalsmith('.')
+	.use(metalsmithSvelte({sourceMap: 'inline'}))
+	.run({'☺️.svelte': {contents: Buffer.from('<div />')}}, (err, files) => {
+		t.equal(err, null, 'should support .svelte file extension.');
+		t.ok(
+			String(files['☺️.js'].contents).split('\n').splice(-2)[0].startsWith('//# sourceMappingURL=data:application/json;base64,'),
+			'should append Base64-encoded source map when `sourceMap` option is \'inline\'.'
+		);
+	});
 
-  new Metalsmith('.')
-  .source('custom_source_directory')
-  .use(metalsmithSvelte({sourceMap: false}))
-  .run({'FOO.HTML': {contents: Buffer.from('</>')}}, ({name, filename}) => {
-    t.equal(
-      name,
-      'ParseError',
-      'should fail when svelte cannot transpile the code.'
-    );
+	new Metalsmith('.')
+	.source('custom_source_directory')
+	.use(metalsmithSvelte({sourceMap: false}))
+	.run({'FOO.HTML': {contents: Buffer.from('</>')}}, ({name, filename}) => {
+		t.equal(
+			name,
+			'ParseError',
+			'should fail when svelte cannot transpile the code.'
+		);
 
-    t.equal(
-      filename,
-      join(__dirname, 'custom_source_directory', 'FOO.HTML'),
-      'should include filename to the error object.'
-    );
-  });
+		t.equal(
+			filename,
+			join(__dirname, 'custom_source_directory', 'FOO.HTML'),
+			'should include filename to the error object.'
+		);
+	});
 });
 
 test('metalsmith-svelte', t => {
-  t.throws(
-    () => metalsmithSvelte(-0),
-    /^TypeError.*Expected an <Object> to set Svelte compiler options, but got -0 \(number\)\./u,
-    'should throw an error when it takes a non-Object argument.'
-  );
+	t.throws(
+		() => metalsmithSvelte(-0),
+		/^TypeError.*Expected an <Object> to set Svelte compiler options, but got -0 \(number\)\./u,
+		'should throw an error when it takes a non-Object argument.'
+	);
 
-  t.throws(
-    () => metalsmithSvelte({sourceMap: [1]}),
-    /^TypeError.*Expected `sourceMap` option to be true, false or 'inline', but got \[ 1 \] \(array\)\./u,
-    'should throw an error when it takes an invalid `sourceMap` option value.'
-  );
+	t.throws(
+		() => metalsmithSvelte({sourceMap: [1]}),
+		/^TypeError.*Expected `sourceMap` option to be true, false or 'inline', but got \[ 1 \] \(array\)\./u,
+		'should throw an error when it takes an invalid `sourceMap` option value.'
+	);
 
-  t.throws(
-    () => metalsmithSvelte({}, {}),
-    /^RangeError.*Expected 0 or 1 argument \(<Object>\), but got 2 arguments\./u,
-    'should throw an error when it takes too many arguments.'
-  );
+	t.throws(
+		() => metalsmithSvelte({}, {}),
+		/^RangeError.*Expected 0 or 1 argument \(<Object>\), but got 2 arguments\./u,
+		'should throw an error when it takes too many arguments.'
+	);
 
-  t.end();
+	t.end();
 });
